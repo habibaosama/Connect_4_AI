74446d10967b2aa945782388db503435cb646d45
MaiAhmedHussein <mayoyaahmed66@gmail.com>
2022-11-10 13:22:16 +0200

    adding Classes

---
+++ src/main/java/com/example/connect_4_ai/MinimaxAlgorithms/MinimaxAgent.java
@@ -0,0 +1,5 @@
+package com.example.connect_4_ai.MinimaxAlgorithms;
+
+public abstract class MinimaxAgent {
+    //Call for making Decisions at any state
+}
---
+++ src/main/java/com/example/connect_4_ai/MinimaxAlgorithms/MinimaxWithPruning.java
@@ -0,0 +1,4 @@
+package com.example.connect_4_ai.MinimaxAlgorithms;
+
+public class MinimaxWithPruning {
+}
